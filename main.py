bd81ae61347b2b05d05ffa334faaadbbb9646b47
Tim <tim@adipid.no>
2019-09-30 17:53:45 +0200

    Can now check multiple products, can also see difference between the prices #1 and #2

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import price_checker
+import json
 
 
 def main():
-    price = price_checker.get_lowest_price("https://prisguiden.no/produkt/logitech-mx-master-3-399986")
-    print(price)
+    products = load_json("products.json")
+    #price = price_checker.get_lowest_price("https://prisguiden.no/produkt/logitech-mx-master-3-399986")
+    #print(price)
+
+    for i in range(len(products["products"])):
+        url = products["products"][i]["url"]
+        purchase_price = products["products"][i]["purchase_price"]
+        #test = price_checker.check_price(url)
+        print(price_checker.compare_prices(url, purchase_price))
+
+
+def load_json(filename):
+    with open(filename) as json_file:
+        imported_file = json.load(json_file)
+        return imported_file
 
 
 if __name__ == '__main__':
--- price_checker.py
+++ price_checker.py
@@ -3,20 +3,45 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
+soup = ""
+
 
 # Returns the lowest price of the product in the URL
-def get_lowest_price(url):
-    response = requests.get(url)
+def check_price(url):
+    soup = get_request(url)
 
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    product_name = soup.find('h1', attrs={'class': 'product-title'})
     lowest_price_shop = soup.find_all('div', attrs={'class': 'product-price'})
 
-    name = product_name.text.strip()
+    name = get_product_name()
     price = lowest_price_shop[0]["data-price"]
 
-    name_price = name + ", " + price
+    return price
 
-    return name_price
 
+def compare_prices(url, old_price):
+    old_price = int(old_price)
+    lowest_price = int(check_price(url))
+
+    if old_price > lowest_price:
+        difference = old_price - lowest_price
+    else:
+        difference = lowest_price - old_price
+
+    return get_product_name() + ", " + str(difference)
+
+
+def get_request(url):
+    response = requests.get(url)
+    global soup
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup
+
+
+# Cannot be used unless the get_request has been initialized
+def get_product_name():
+    global soup
+
+    product_name = soup.find('h1', attrs={'class': 'product-title'})
+    name = product_name.text.strip()
+
+    return name
