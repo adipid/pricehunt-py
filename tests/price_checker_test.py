460ee3fc06c2e8b1690cede9eb6682aef11be2c6
Tim <tim@adipid.no>
2020-01-14 19:19:47 +0100

    Added real unit tests

--- tests/price_checker_test.py
+++ tests/price_checker_test.py
@@ -1,9 +1,16 @@
 import unittest
 
+import price_checker
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class PriceCheckerTest(unittest.TestCase):
+    def test_get_lowest_price(self):
+        price = price_checker.get_lowest_price("https://prisguiden.no/produkt/logitech-mx-master-3-399986")
+        self.assertEqual("999", price)
+
+    def test_compare_prices(self):
+        price = price_checker.compare_prices("https://prisguiden.no/produkt/logitech-mx-master-3-399986", "1199")
+        self.assertEqual("Logitech MX Master 3, 200", price)
 
 
 if __name__ == '__main__':
