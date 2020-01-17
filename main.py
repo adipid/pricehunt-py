a7c5e865fd51d8290be27be8c32a0d38a4053a97
Tim <tim@adipid.no>
2020-01-17 19:48:11 +0100

    Added some docstrings for some functions #11
    Also made the script update both lists when performing add/remove, also fixed index-bug when removing

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ def main():
 
 
 def menu():
-    print("************MAIN MENU**************\n")
+    print("************MAIN MENU**************\n"
+          "Please enter a number\n0")
     choice = input("1: Add new product(s) to the list\n"
                    "2: Remove product(s) from the list\n"
                    "3: See details from the list\n"
@@ -32,7 +33,7 @@ def menu():
     elif choice == "2":
         remove_product()
     elif choice == "3":
-        display_list()
+        detailed_list()
     elif choice == "4" or choice.lower() == "q":
         sys.exit()
     else:
@@ -41,9 +42,14 @@ def menu():
 
 
 def add_product():
+    """
+    Add product(s) to the product-list and updates
+    products.json
+    """
+
     # Need URL, date, price and shop
     url = input("Please enter prisguiden.no URL:\n").replace(" ", "")
-    date = input("Please enter purchase date: (2020-01-01)\n")
+    date = input("Please enter purchase date: (2020-12-24)\n")
     price = input("Please enter the purchase price: (without spaces)\n")
     shop = input("Please enter the store/shop name:\n")
 
@@ -54,21 +60,37 @@ def add_product():
         "shop": shop
     }
 
+    # Adds the product to the existing list
+    products_list.append(Product(new_product))
+
+    # Adds the product to the products.json data
     products_data.append(new_product)
 
+    # Updates products.json with the new product
     with open("products.json", "w") as json_file:
         json.dump(products_data, json_file, indent=2)
 
 
 def remove_product():
-    remove_index = input("Which product do you want to remove? ")
+    """
+    Removes product(s) from the product-list
+    and updates products.json
+    """
+
+    remove_index = int(input("Which product do you want to remove? ")) - 1
+
+    # Removes the product from existing list
+    products_list.pop(int(remove_index))
+
+    # Removes the product from the products.json data
     products_data.pop(int(remove_index))
 
+    # Updates products.json with the removed product
     with open("products.json", "w") as json_file:
         json.dump(products_data, json_file, indent=2)
 
 
-def display_list():
+def detailed_list():
     index = 1
     for product in products_list:
         print(str(index) + "\t" + "Name: " + product.name +
