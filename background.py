2d5a5685be4f3e2f85b87bc4c00a751872f21652
Tim <tim@adipid.no>
2020-02-24 21:34:50 +0100

    Added a new field in .json file, last checked price
    This is for safe keeping, so that when you  dont have to keep getting notifications in the background

--- background.py
+++ background.py
@@ -1,18 +1,21 @@
 import json
 import pync
-import time
 
 import setup
 
+SCRIPT_NAME = "Pricehunt-py"
 
 setup.product_init()
+product_data = setup.load_json(r"" + setup.cwd + "/" + "products.json")
 
-pync.notify("Started the background script!", title="Pricehunt-py")
+pync.notify("Started the background script!", title=SCRIPT_NAME)
 
-for product in setup.products_list:
-    if int(product.last_check_price) == int(product.price_difference):
+for index, product in enumerate(setup.products_list):
+    if int(product_data[index]["last_checked_price"]) == int(product.price_difference):
         break
 
     if int(product.purchased_price) > int(product.price):
-        pync.notify(product.name + " price has changed!", title="Pricehunt-py", open=product.url)
-        product.last_check_price = int(product.price_difference)
+        pync.notify(product.name + " price has changed!", title=SCRIPT_NAME, open=product.url)
+        product_data[index]["last_checked_price"] = int(product.price_difference)
+        with open('products.json', 'w') as f:
+            json.dump(product_data, f, indent=2)
